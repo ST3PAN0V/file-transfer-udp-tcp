baaf7af6ef43cc2f6cd0110130a38c533066cf2d
ArtemStepanov <piprusova@gmail.com>
2025-01-15 11:02:46 +0300

    refactor: add config.py file. Delete test artifacts

--- src/client.py
+++ src/client.py
@@ -1,39 +1,25 @@
+from config import *
+
 import socket
 from os.path import abspath
 import sys
 import time
 
-SERVER_HOST = '127.0.0.1'
-SERVER_PORT = 8080
-BUFFER = 4096
-TIMEOUT = 5 # seconds
-
 class UDPClient:
-    """
-    A UDP client for receiving files from a server.
-    """
+    """A UDP client for receiving files from a server."""
 
-    def __init__(
-                 self, 
-                 file_path: str, 
-                 host: str = SERVER_HOST, 
-                 port: int = SERVER_PORT
-                 ):
-        """
-        Init the UDP client with the file_path, host, and port.
-        """
+    def __init__(self, file_path: str, host: str = SERVER_HOST, port: int = SERVER_PORT):
+        """Init the UDP client with the file_path, host, and port."""
         self.file_path = file_path
         self.host = host
         self.port = port
         self.sock = None
 
     def _receive_file(self):
-        """
-        Receives content from the server and writes it to the file.
-        """
+        """Receives content from the server and writes it to the file."""
         try:
             with open(self.file_path, 'wb') as file:
-                print(f"downloading...")
+                print("downloading...")
                 start_time = time.time()
                 while True:
                     try:
@@ -42,37 +28,32 @@ class UDPClient:
                         if not data:
                             break
                         file.write(data)
-                        start_time = time.time()  # Reset timer
+                        start_time = time.time() # Reset timer
                     except socket.timeout:
                         break
-                print("downloaded as", abspath(self.file_path), flush=True)
+
+                print(f"downloaded as {abspath(self.file_path)}", flush=True)
         except Exception as exc:
-            print(f"Err: {exc}")
+            print(f"Error: {exc}")
 
     def run(self):
-        """
-        Connects to the server and receiving file.
-        """
+        """Connects to the server and receives the file."""
         try:
-            self.sock = socket.socket(
-                                      socket.AF_INET, 
-                                      socket.SOCK_DGRAM
-                                      )
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             self.sock.sendto(b"init", (self.host, self.port))
             print(f"requesting from {self.host}:{self.port}")
             self._receive_file()
         except Exception as exc:
-            print(f"Err: {exc}")
+            print(f"Error: {exc}")
         finally:
             self.sock.close()
 
+
 def main():
-    """
-    Main function. Start client.
-    """
+    """Main function. Start client."""
     if len(sys.argv) != 2:
         print("Usage: python client.py <file_name>")
-        return
+        return 1
 
     file_path = sys.argv[1]
     client = UDPClient(file_path=file_path)
--- src/server.py
+++ src/server.py
@@ -1,34 +1,21 @@
+from config import *
+
 import socket
 from os.path import abspath
 import sys
 
-SERVER_HOST = '127.0.0.1'
-SERVER_PORT = 8080
-BUFFER = 4096
-
 class UDPServer:
-    """
-    A UDP server for sending file to a client.
-    """
+    """A UDP server for sending file to a client."""
 
-    def __init__(
-                 self,
-                 file_path: str, 
-                 host: str = SERVER_HOST, 
-                 port: int = SERVER_PORT
-                 ):
-        """
-        Init the UDP server with the filename, host, and port.
-        """
+    def __init__(self, file_path: str, host: str = SERVER_HOST, port: int = SERVER_PORT):
+        """Init the UDP server with the filename, host, and port."""
         self.file_path = file_path
         self.host = host
         self.port = port
         self.sock = None
 
     def _send_file(self, addr: tuple):
-        """
-        Sends the content of a file to the client socket.
-        """
+        """Sends the content of a file to the client socket."""
         try:
             with open(self.file_path, 'rb') as file:
                 print("sending...")
@@ -37,38 +24,33 @@ class UDPServer:
                     if not data:
                         break
                     self.sock.sendto(data, addr)
-            print("finished sending to", f"{addr[0]}:{addr[1]}")
-        except Exception as e:
-            print(f"Err: {e}")
+                print(f"finished sending to {addr[0]}:{addr[1]}")
+        except FileNotFoundError:
+            print(f"Error: File '{self.file_path}' not found")
+        except Exception as exc:
+            print(f"Error: {exc}")
 
     def run(self):
-        """
-        Starts the UDP server, waiting for a client connection,
-        and sends the file's data.
-        """
+        """Starts the UDP server, waits for a client connection, and sends the file's data."""
         try:
-            self.sock = socket.socket(
-                                      socket.AF_INET,
-                                      socket.SOCK_DGRAM
-                                      )
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             self.sock.bind((self.host, self.port))
-            print("serving", abspath(self.file_path), flush=True)
+            print(f"serving {abspath(self.file_path)}", flush=True)
             data, addr = self.sock.recvfrom(BUFFER)
-            print("request from", f"{addr[0]}:{addr[1]}")
-            self._send_file(addr=addr)
+            print(f"request from {addr[0]}:{addr[1]}")
+            self._send_file(addr)
         except Exception as exc:
-            print(f"Err: {exc}")
+            print(f"Error: {exc}")
         finally:
             self.sock.close()
 
 
 def main():
-    """
-    Main function. Start server.
-    """
+    """Main function. Start server."""
+
     if len(sys.argv) != 2:
         print("Usage: python server.py <file_name>")
-        return
+        return 1
 
     file_path = sys.argv[1]
     server = UDPServer(file_path=file_path)
@@ -78,4 +60,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
